a97165e23f38575d6c048d21ab2d5b049b6721da
Ada Cakmak <nevinadacakmak@hotmail.com>
2026-03-24 12:32:21 -0400

    day 3: fixed the ptr issue

--- main.c
+++ main.c
@@ -17,7 +17,6 @@ int main(){
 
     while (fgets(input, sizeof(input), stdin) != NULL)
     {
-        memset(argv, 0, sizeof(argv));
         input[strcspn(input, "\n")] = '\0';
         //issue1: what if someone wants to put multiple lines?
         //issue2: just pressing enter, undefined behaviour.
@@ -30,58 +29,59 @@ int main(){
         i++;
         while (token != NULL)
         {
-            printf("current token: %s\n",token);
+            printf("prev token: %s\n",token);
 
             token = strtok(NULL, delim); //need this NULL call to keep going, according to strtok stackoverflow
 
             argv[i] = token;
-            printf("i=%d token=%s\n", i, token); //issue here
+            printf("current i=%d token=%s\n", i, token); //issue here
             i++;
             
         }
         argv[i] = NULL;
 
         //execvp cant handle cd.
-        printf("arg0 %s arg1 %s",argv[0],argv[1]);
-        printf("hello");
 
         if (strcmp(argv[0],"cd")==0)
         {
-            printf("hello");
-            argv[1]=strcat("/",argv[1]);
+            //char address[1024] = "/";
+            //strcat(address,argv[1]);
+            //strcpy(argv[1],address);
             chdir(argv[1]);
+            printf("chdir with %s\n",argv[1]);
         }
-
-        if(!strcmp(argv[0],"exit"))
+        else if(strcmp(argv[0],"exit")==0)
         {
             return 0;
         }
-
-        //fork, execvp, wait
-        int c_pid = 0;
-        c_pid = fork();
-        if (c_pid == 0) //child
+        else
         {
-            printf("this is the child\n");
-
-            int iserr = 0;
-            iserr = execvp(argv[0],argv);
-
-            if (iserr == -1) 
+            //fork, execvp, wait
+            int c_pid = 0;
+            c_pid = fork();
+            if (c_pid == 0) //child
             {
-                perror("problem with execvp, argv[]\n");
-                printf("current argv[0]: %s\n",argv[0]);
-            }  
-        }
-        else if (c_pid == -1)
-        {
-            perror("error with child process creation!\n");
-        }
-        else //parent
-        {
-            printf("in the parent, PID = %d\n",c_pid);
-            int childstatus;
-            wait(&childstatus);
+                printf("this is the child\n");
+
+                int iserr = 0;
+                iserr = execvp(argv[0],argv);
+
+                if (iserr == -1) 
+                {
+                    perror("problem with execvp, argv[]\n");
+                    printf("current argv[0]: %s\n",argv[0]);
+                }  
+            }
+            else if (c_pid == -1)
+            {
+                perror("error with child process creation!\n");
+            }
+            else //parent
+            {
+                printf("in the parent, PID = %d\n",c_pid);
+                int childstatus;
+                wait(&childstatus);
+            }
         }
     }
 
